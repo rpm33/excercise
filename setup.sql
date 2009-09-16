6d34fcc21ebb66ffdf45ddf49f1136f9214f5e45
reon <reon@kakusan.well-ness.jp>
2009-09-16 16:31:28 +0900

    added a clumn

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `email` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -9,5 +9,7 @@ CREATE TABLE `message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `subject` text NOT NULL,
   `body` text NOT NULL,
+  `user_email` varchar(64),
+  FOREIGN KEY ( `user_email` ) REFERENCES `user` ( `email` ),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
