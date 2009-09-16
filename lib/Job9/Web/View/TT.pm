7e8bcc354ae24dd0592920f84d38d3de6932c338
Taro Funaki <t@33rpm.jp>
2009-09-16 16:25:01 +0900

    add TT view

---
+++ lib/Job9/Web/View/TT.pm
@@ -0,0 +1,34 @@
+package Job9::Web::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    WRAPPER            => 'wrapper.tt',
+);
+
+=head1 NAME
+
+Job9::Web::View::TT - TT View for Job9::Web
+
+=head1 DESCRIPTION
+
+TT View for Job9::Web. 
+
+=head1 SEE ALSO
+
+L<Job9::Web>
+
+=head1 AUTHOR
+
+船木大郎
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
