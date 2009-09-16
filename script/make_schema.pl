cf17e1fb89f1dfc6ecf2d85a34db4df7cb89a276
Taro Funaki <t@33rpm.jp>
2009-09-16 16:06:27 +0900

    add make_schema-script

---
+++ script/make_schema.pl
@@ -0,0 +1,15 @@
+#!/usr/local/bin/perl 
+
+use strict;
+use warnings;
+
+use DBIx::Class::Schema::Loader qw(make_schema_at);
+
+make_schema_at(
+    'Job9::Schema',
+    {
+        debug          => 1,
+        dump_directory => './lib',
+    },
+    [ qw(dbi:mysql:dbname=test) ],
+);
